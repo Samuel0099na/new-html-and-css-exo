aad18e38d0635dd7825cf2ed6e6914e6f0d324dd
Samuel0099na <samuelnaphard@gmail.com>
2023-11-27 21:42:51 -0400

    new commit of html and css exo

---
+++ index.js
@@ -0,0 +1,6 @@
+const btn1 = document.getElementById("bntn-1");
+const btn2 = document.getElementById("bntn-2");
+
+btn1.addEventListener("mouseover", () => {
+  if (btn2.classList.toggle("classlist-btn1"));
+});
